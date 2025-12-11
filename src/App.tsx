eac0d12bd5c6c8aa5c5a5d90af6049b158daa6ab
IslamTariq <islamtriq@gmail.com>
2025-12-11 01:10:39 +0100

    code formatter integrated, code formatted

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import CanvasGrid from './components/CanvasGrid'
+import CanvasGrid from "./components/CanvasGrid";
 
 function App() {
   return (
     <div className="App">
       <CanvasGrid rows={10} cols={10} cellSize={40} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
